453376e536c0731e1e1afc826ede8f7ea82453bb
cimentadaj <cimentadaj@gmail.com>
2017-10-30 17:29:50 +0100

    Finished grabbing ess round links for separate countries

--- R/ess_country_url.R
+++ R/ess_country_url.R
@@ -73,7 +73,7 @@ ess_country_url <- function(country, rounds) {
     #         c("Slovakia", "Slovenia", "Spain", "Sweden"),
     #         c("Switzerland", "Turkey", "Ukraine", "United Kingdom"))
     
-
+    
     # While the chosen index is NOT a valid country,
     # promt a country list with the country numbers
     # until the user chooses a new country
@@ -105,10 +105,12 @@ ess_country_url <- function(country, rounds) {
   # Extract html from country link to donwnload rounds
   country_rounds <- httr::GET(chosen_country_link)
   
-  # Find only the name of the round
-  dirty_round_names <- xml2::xml_find_all(xml2::read_html(country_rounds), "//h2")
+  country_round_html <- xml2::read_html(country_rounds)
   
-  # Clean it from it's attributes
+  # Find only the name of the round
+  dirty_round_names <- xml2::xml_find_all(country_round_html, "//h2")
+  
+  # Clean the dirty round name fromit's attributes
   round_names <-
     sapply(
       X = as.character(dirty_round_names),
@@ -130,25 +132,58 @@ ess_country_url <- function(country, rounds) {
   # If any ESS round is missing
   if(any(!rounds %in% available_rounds)) {
     stop("Only rounds ",
-         paste0(available_rounds, collapse = "-"),
+         paste0("ESS", available_rounds, collapse = ", "),
          " available for ", country)
   }
   
   
-  XML::htmlParse(country_rounds, asText = TRUE)
+  country_node <-
+    xml2::xml_find_all(
+      xml2::xml_find_all(
+        xml2::xml_find_all(
+          country_round_html, # Go deep down into the node
+          '//ul'),
+        "//li"),
+      "//a")
   
-  for (index in seq_along(round_links)) {
-    download.page <- httr::GET(paste0("http://www.europeansocialsurvey.org", 
-                                      round_links[index]))
-    download.block <- XML::htmlParse(download.page, asText = TRUE)
-    z <- XML::xpathSApply(download.block, "//a", function(u) XML::xmlAttrs(u)["href"])
-    stata.files[index] <- z[grep("stata", z)]
-  }
-  # } # this bracket closes the loop commented aout from above
+  # Here we have all href from the website
+  country_href <- xml2::xml_attrs(country_node, "href")
   
-  full_urls <- sort(paste0("http://www.europeansocialsurvey.org", stata.files))
+  # Only select the ones that match this:
+  # /download.html\\?file= for the downoad section
+  # ESS[0-9] for any of the rounds
+  # [A-Z]{1,1} followed by a capital letter for
+  # the country name, as in DK
+  # (.*) anything in between
+  # [0-9]{4, } for the year of round
+  incomplete_links <-
+    sort(grep("^/download.html\\?file=ESS[0-9][A-Z]{1,2}(.*)[0-9]{4, }$",
+              country_href,
+              value = TRUE))
   
-  full_urls
+  # Extract round numbers
+  round_numbers <- as.numeric(stringr::str_extract(incomplete_links, "[0-9]{1,2}"))
+  
+  # Build final ESS round links
+  round_links <-
+    paste0(
+      ess_website,
+      incomplete_links[which(round_numbers %in% rounds)]
+    )
+  round_links
+  
+  # for (index in seq_along(round_links)) {
+  #   download.page <- httr::GET(paste0("http://www.europeansocialsurvey.org", 
+  #                                     round_links[index]))
+  #   download.block <- XML::htmlParse(download.page, asText = TRUE)
+  #   z <- XML::xpathSApply(download.block, "//a", function(u) XML::xmlAttrs(u)["href"])
+  #   stata.files[index] <- z[grep("stata", z)]
+  # }
+  # # } # this bracket closes the loop commented aout from above
+  # 
+  # full_urls <- sort(paste0("http://www.europeansocialsurvey.org", stata.files))
+  # 
+  # full_urls
 }
 
 
