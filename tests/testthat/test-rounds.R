1abbeb71c08ccb7a4dfa7926c86020d27c16636c
cimentadaj <cimentadaj@gmail.com>
2017-10-30 11:03:48 +0100

    Forgot to add files to: changed the word waves to rounds for every function/test/documentation/readme

--- tests/testthat/test-rounds.R
+++ tests/testthat/test-rounds.R
@@ -1,6 +1,6 @@
 
 # Test for only one wave
-wave_one <- ess_waves(1, "cimentadaj@gmail.com")
+wave_one <- ess_rounds(1, "cimentadaj@gmail.com")
 
 # check is list
 expect_is(wave_one, "list")
@@ -8,7 +8,7 @@ expect_is(wave_one, "list")
 # check is length one
 expect_length(wave_one, 1)
 
-# check that ess_waves returns data frames
+# check that ess_rounds returns data frames
 expect_is(wave_one[[1]], "data.frame")
 
 # check that the number of rows is greater than 0
@@ -18,46 +18,46 @@ expect_gt(nrow(wave_one[[1]]), 0)
 expect_gt(ncol(wave_one[[1]]), 0)
 
 
-# Test for all waves
-all_waves <- ess_waves(1:7, "cimentadaj@gmail.com")
+# Test for all rounds
+all_rounds <- ess_rounds(1:7, "cimentadaj@gmail.com")
 
 # check is list
-expect_is(all_waves, "list")
+expect_is(all_rounds, "list")
 
 # check is length one
-expect_length(all_waves, 7)
+expect_length(all_rounds, 7)
 
 # check that all ess returns data frames
-expect_equal(all(sapply(all_waves, function(x) "data.frame" %in% class(x))), TRUE)
+expect_equal(all(sapply(all_rounds, function(x) "data.frame" %in% class(x))), TRUE)
 
 # check that all data frames have more than 0 rows
-expect_equal(all(sapply(all_waves, nrow) > 0), TRUE)
+expect_equal(all(sapply(all_rounds, nrow) > 0), TRUE)
 
 # check that all data frames have more than 0 columns
-expect_equal(all(sapply(all_waves, ncol) > 0), TRUE)
+expect_equal(all(sapply(all_rounds, ncol) > 0), TRUE)
 
 # Test for only downloads
 
 # Test whether you get a message where the downloads are at
-which_waves <- 2
+which_rounds <- 2
 
-expect_message(downloads <- ess_waves(1:which_waves, "cimentadaj@gmail.com", only_download = TRUE),
+expect_message(downloads <- ess_rounds(1:which_rounds, "cimentadaj@gmail.com", only_download = TRUE),
               "All files saved to")
 
 # Test whether the downloaded files are indeed there
 ess_files <- list.files(downloads, pattern = "ESS", recursive = TRUE)
 
-# Same number of stata files as the waves attempted
+# Same number of stata files as the rounds attempted
 # to download?
-expect_equal(sum(grepl(".dta", ess_files)), which_waves)
+expect_equal(sum(grepl(".dta", ess_files)), which_rounds)
 
-# Same number of zip files as the waves attempted
+# Same number of zip files as the rounds attempted
 # to download?
-expect_equal(sum(grepl(".zip", ess_files)), which_waves)
+expect_equal(sum(grepl(".zip", ess_files)), which_rounds)
 
-# Same number of do files as the waves attempted
+# Same number of do files as the rounds attempted
 # to download?
-expect_equal(sum(grepl(".do", ess_files)), which_waves)
+expect_equal(sum(grepl(".do", ess_files)), which_rounds)
 
 # Delete all downloaded files
 unlink(downloads, recursive = TRUE, force = TRUE)
--- tests/testthat/tests-download_rounds_stata.R
+++ tests/testthat/tests-download_rounds_stata.R
@@ -1,8 +1,8 @@
-# These are the functions which are actually doing the work of testing this, not ess_waves
+# These are the functions which are actually doing the work of testing this, not ess_rounds
 # For wrong emails, test it will through error
-expect_error(download_waves_stata(1, "random@email.morerandom"),
+expect_error(download_rounds_stata(1, "random@email.morerandom"),
              "email address you provided is not associated with any registered")
 
 # Test it will throw error when wave is not available
-expect_error(download_waves_stata(c(1, 22), "cimentadaj@gmail.com"),
+expect_error(download_rounds_stata(c(1, 22), "cimentadaj@gmail.com"),
              "ESS round [0-9]+ is not a available at")
