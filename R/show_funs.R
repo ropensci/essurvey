c2a0d41b7305a288ae0ae82c6da71a09c26c33a0
cimentadaj <cimentadaj@gmail.com>
2017-12-02 20:23:38 +0100

    Move show_rounds to show_funs.R

--- R/show_funs.R
+++ R/show_funs.R
@@ -1,3 +1,25 @@
+#' Return available rounds in the European Social Survey
+#'
+#' @return numeric vector with available rounds
+#' @export
+#'
+#' @examples
+#' 
+#' show_rounds()
+#' 
+show_rounds <- function() {
+  incomplete_links <- grab_rounds_link()
+  
+  # extract ESS* part to detect dupliacted
+  ess_prefix <- sort(stringr::str_extract(incomplete_links, "ESS[:digit:]"))
+  
+  # extract only the digit
+  unique_rounds_available <- unique(stringr::str_extract(ess_prefix, "[:digit:]"))
+  
+  as.numeric(unique_rounds_available)
+}
+
+
 #' Return available countries in the European Social Survey
 #'
 #' @return character vector with available countries
