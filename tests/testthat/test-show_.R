843fde7e9efbdc5617ede49cd7af35d26af69cd5
cimentadaj <cimentadaj@gmail.com>
2017-12-03 18:14:54 +0100

    Add tests for *_themes variables

--- tests/testthat/test-show_.R
+++ tests/testthat/test-show_.R
@@ -1,3 +1,14 @@
+# show_rounds
+test_that("show_rounds returns correct output", {
+  # Check whether show_countries is character
+  expect_is(show_rounds(), "numeric")
+  
+  # Check whether show_countries has length greater than 0
+  expect_gt(length(show_rounds()), 0)
+  
+  # Check there are no duplicate rounds
+  expect_false(all(duplicated(show_rounds())))
+})
 
 # show_countries
 test_that("show_countries returns correct output", {
@@ -24,14 +35,38 @@ test_that("show_country_rounds returns correct output", {
   expect_false(all(duplicated(show_country_rounds("United Kingdom"))))
 })
 
-# show_rounds
-test_that("show_rounds returns correct output", {
+# show_themes()
+test_that("show_themes returns correct output", {
   # Check whether show_countries is character
-  expect_is(show_rounds(), "numeric")
+  expect_is(show_themes(), "character")
   
   # Check whether show_countries has length greater than 0
-  expect_gt(length(show_rounds()), 0)
+  expect_gt(length(show_themes()), 0)
   
-  # Check there are no duplicate rounds
-  expect_false(all(duplicated(show_rounds())))
+  # Check there are no duplicate countries
+  expect_false(all(duplicated(show_themes())))
+})
+
+# show_theme_rounds
+test_that("show_theme_rounds returns correct output for rounds  == 1", {
+  # Check whether show_countries is character
+  expect_is(show_theme_rounds("Democracy"), "numeric")
+  
+  # Check whether show_countries has length greater than 0
+  expect_gt(length(show_theme_rounds("Democracy")), 0)
+  
+  # # Check there are no duplicate rounds for countries
+  expect_false(all(duplicated(show_theme_rounds("Democracy"))))
+})
+
+# show_theme_rounds
+test_that("show_theme_rounds returns correct output for rounds > 1", {
+  # Check whether show_countries is character
+  expect_is(show_theme_rounds("Politics"), "numeric")
+  
+  # Check whether show_countries has length greater than 0
+  expect_gt(length(show_theme_rounds("Politics")), 0)
+  
+  # # Check there are no duplicate rounds for countries
+  expect_false(all(duplicated(show_theme_rounds("Politics"))))
 })
