19cd2db9e926e73283c4ae147370407827b577ab
cimentadaj <cimentadaj@gmail.com>
2017-12-03 18:40:39 +0100

    Brought back extract_html and get_ref which were used to get country urls

--- R/ess_country_url.R
+++ R/ess_country_url.R
@@ -7,7 +7,7 @@ ess_country_url <- function(country, rounds, format) {
   
   # Returns each countries href attribute with its website
   # ess_website is a vector set as metadata
-  available_countries <- show_countries()
+  available_countries <- .global_vars$countries
   
   if (!country %in% available_countries) {
     stop("Country not available in ESS. Check show_countries()")
@@ -65,4 +65,53 @@ ess_country_url <- function(country, rounds, format) {
   full_urls <- sort(paste0(.global_vars$ess_website, format.files))
   
   full_urls
+}
+
+# Function accepts the chosen country and the list
+# of all available countries and returns the html doc
+# for the chosen country that contains the whole list of
+# links to download that countries rounds.
+extract_html <- function(chosen_module, available_modules, module_index) {
+  
+  # Returns "/data/country.html?c=ukraine" for all countries
+  all_module_links <-
+    xml2::xml_attr(
+      get_href(.global_vars$ess_website, module_index),
+      "href"
+    )
+  
+  # Build full url for chosen country
+  chosen_module_link <-
+    paste0(
+      .global_vars$ess_website,
+      all_module_links[which(chosen_module == available_modules)] # index where the country is at
+    )
+  
+  # Extract html from country link to donwnload rounds
+  module_rounds <- httr::GET(chosen_module_link)
+  
+  module_round_html <- xml2::read_html(module_rounds)
+  
+  module_round_html
+}
+
+# Function to grab <a href="/data/country.html?c=latvia">Latvia</a>
+# for each country
+get_href <- function(ess_website, module_index) {
+  download_page <- httr::GET(paste0(ess_website, module_index))
+  download_block <- XML::htmlParse(download_page, asText = TRUE)
+  z <- XML::xpathSApply(download_block, "//a", function(u) XML::xmlAttrs(u)["href"])
+  
+  # Get the <a href="/data/country.html?c=latvia">Latvia</a> for each country
+  country_node <- xml2::xml_find_all(xml2::read_html(download_page), '//td [@class="label"]//a')
+  
+  country_node
+}
+
+# Function to automatically clean attributes such as
+# <a> something <\a>. This will clean to: something
+# if a and /a are specified as arguments.
+clean_attr <- function(x, ...) {
+  other_attrs <- paste0((list(...)), collapse = "|")
+  gsub(paste0(">|", "<|", other_attrs), "", x)
 }
