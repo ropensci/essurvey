b47120f5d3b63058cc8d4612db6d215f9a777837
Jorge Cimentada <cimentadaj@gmail.com>
2019-08-23 12:32:41 +0200

    Debug Travis

--- tests/testthat/test-set_email.R
+++ tests/testthat/test-set_email.R
@@ -6,4 +6,7 @@ test_that("test email is correctly set and can be overwritten", {
   expect_equal(Sys.getenv("ESS_EMAIL"), "test@test.com")
   set_email(ess_email = "test_bis@test.com")
   expect_equal(Sys.getenv("ESS_EMAIL"), "test_bis@test.com")
+
+  # Reset
+  Sys.setenv("ESS_EMAIL" = "")
 })
