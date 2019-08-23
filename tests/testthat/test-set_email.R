fadbca798dd7e000a3939b40ca6150b05624ccbc
Jorge Cimentada <cimentadaj@gmail.com>
2019-08-23 12:45:31 +0200

    Debug travis fruther

--- tests/testthat/test-set_email.R
+++ tests/testthat/test-set_email.R
@@ -2,11 +2,13 @@ context("test-set_email")
 
 # Test for set_email
 test_that("test email is correctly set and can be overwritten", {
+  old_env_var <- Sys.getenv("ESS_EMAIL")
+  
   set_email(ess_email = "test@test.com")
   expect_equal(Sys.getenv("ESS_EMAIL"), "test@test.com")
   set_email(ess_email = "test_bis@test.com")
   expect_equal(Sys.getenv("ESS_EMAIL"), "test_bis@test.com")
 
   # Reset
-  Sys.setenv("ESS_EMAIL" = "")
+  Sys.setenv("ESS_EMAIL" = old_env_var)
 })
