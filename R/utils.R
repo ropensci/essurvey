275d6826bd97916f17ad59bd9c0f1ea8c085b8e3
Jorge Cimentada <cimentadaj@gmail.com>
2019-09-23 17:08:17 +0200

    Adds citation with new add_citation

---
+++ R/utils.R
@@ -0,0 +1,4 @@
+.onAttach <- function(libname, pkgname) {
+   packageStartupMessage("\nPlease cite as: \n")
+   packageStartupMessage("Cimentada, Jorge (2019). Download Data from the European Social Survey on the Fly R package version 1.0.2.")
+}
